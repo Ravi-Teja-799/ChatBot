8639c9381a6d8155112a97b749b670940cff0a72
Ravi-Teja-799 <87001298+Ravi-Teja-799@users.noreply.github.com>
2021-12-05 10:20:43 +0530

    Create chatbot.js

---
+++ chatbot.js
@@ -0,0 +1,34 @@
+let chatbotMsgList = ["Hi", "Hey", "Good Morning", "Good Evening", "How can I help you?", "Thank You"];
+
+let chatContainerEl = document.getElementById('chatContainer');
+let userInputEl = document.getElementById("userInput");
+
+function sendMsgToChatbot() {
+    let userMsg = userInputEl.value;
+
+    let msgContainerEl = document.createElement("div");
+    msgContainerEl.classList.add("msg-to-chatbot-container");
+    chatContainerEl.appendChild(msgContainerEl);
+
+    let userMsgEl = document.createElement("span");
+    userMsgEl.textContent = userMsg;
+    userMsgEl.classList.add("msg-to-chatbot");
+    msgContainerEl.appendChild(userMsgEl);
+
+    userInputEl.value = "";
+    getReplyFromChatbot();
+}
+
+function getReplyFromChatbot() {
+    let noOfChatbotMsgs = chatbotMsgList.length;
+    let chatbotMsg = chatbotMsgList[Math.ceil(Math.random() * noOfChatbotMsgs) - 1];
+
+    let msgContainerE1 = document.createElement("div");
+    msgContainerE1.classList.add("msg-from-chatbot-container");
+    chatContainerEl.appendChild(msgContainerE1);
+
+    let chatbotMsgEl = document.createElement("span");
+    chatbotMsgEl.textContent = chatbotMsg;
+    chatbotMsgEl.classList.add("msg-from-chatbot");
+    msgContainerE1.appendChild(chatbotMsgEl);
+}
